e0f400339453cfa665afb3e188b602ee5e4034d8
Paul Norris <paul.norrisii@gmail.com>
2025-05-11 18:13:46 -0700

    My first commit.

--- GitSourceControl/ContentView.swift
+++ GitSourceControl/ContentView.swift
@@ -10,11 +10,13 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Image(systemName: "heart.fill")
+                .font(.largeTitle)
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Swiftful Thinking")
         }
+        .padding()
         .padding()
     }
 }
