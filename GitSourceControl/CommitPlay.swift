38a0b0303e67252f71321795bc863589e42355eb
Paul Norris <paul.norrisii@gmail.com>
2025-05-12 16:40:12 -0700

    added private var

--- GitSourceControl/CommitPlay.swift
+++ GitSourceControl/CommitPlay.swift
@@ -8,6 +8,9 @@
 import SwiftUI
 
 struct CommitPlay: View {
+    
+    @State private var title: String = ""
+    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
         Text("Screen 2")
